06150d4b801e5c2222e4f8d22c4aeb0633a37f5d
Kichekov Vlad <Kichekov@mail.ru>
2024-11-30 00:49:41 +0300

    Task 10

---
+++ src/Task_12.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task_12 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+
+        }
+        System.out.println(sum);
+
+    }
+}
