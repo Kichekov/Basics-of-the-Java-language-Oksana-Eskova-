6ef186be3dc96a7257efe74003e2cc252228e2e1
Kichekov Vlad <kichekov@inbox.lv.>
2024-12-08 00:29:23 +0300

    Task 39

--- src/Task_39.java
+++ src/Task_39.java
@@ -1,8 +1,31 @@
+/*
+Пользователь вводит размер массива и начальное значение генератора случайных чисел.
+Создать массив  чисел типа double заданного размера и заполнить его случайными числами от 0 до 2
+ (не включая). Распечатать исходный массив, используя метод toString() - в квадратных скобках, элементы
+  через запятую.
+С новой строки вывести на консоль сумму максимального и минимального элементов массива.
+Тестовые данные
+Sample Input:
+
+5 25
+Sample Output:
+
+[1.4631897195641836, 0.10745131965527377, 1.2906709676610488, 0.14895347726291952, 0.012141832026828503]
+1.4753315515910121
+ */
+
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Task_39 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();//Пользователь вводит размер массива;
+        int[] a = new int [size];
+        int start = scan.nextInt();
+        Random rand = new Random(start);
+
 
     }
 }
