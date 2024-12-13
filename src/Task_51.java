842ce16f1706f4201c9ecae7d0691607c9d41b42
Kichekov Vlad <kichekov@inbox.lv.>
2024-12-13 20:29:37 +0300

    Task 51

---
+++ src/Task_51.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task_51 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String phrase = "";
+        for (int i = 0; i <3; i++){
+            String str = scan.nextLine();
+            int findFirst = str.indexOf(';')+1;
+            int findLast = str.lastIndexOf(';');
+            String element = str.substring(findFirst, findLast);
+            phrase += element;
+
+        }
+        System.out.println(phrase);
+    }
+}
